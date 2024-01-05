16e2c91809d33bad68133fda957c1af7712cb0e8
Finn Wilkinson <finnwilkinson5@gmail.com>
2024-01-05 11:31:47 +0000

    Began to outline GEMM comparison.

--- main.c
+++ main.c
@@ -13,10 +13,28 @@ int main(int argc, char *argv[]) {
   printf("\tGPU Tests Enabled: %s\n", gpu_enabled_str);
   printf("\tGPU Library: %s\n", GPU_LIB_NAME);
 
-  // Perform all CPU kernels & record results
+  // GEMM Comparison - Square
+  for (uint64_t dim = 1; dim <= UPPER_LIMIT; dim++) {
+    uint64_t M = dim, N = dim, K = dim;
+    // Perform CPU
+    struct dT_Times cpuTimes = {gemm_cpu(_fp16_, ITERATIONS, M, N, K),
+                                gemm_cpu(_fp32_, ITERATIONS, M, N, K),
+                                gemm_cpu(_fp64_, ITERATIONS, M, N, K)};
+    // Perform GPU
+    struct dT_Times gpuTimes_OffloadOnce = {
+        gemm_gpu(_fp16_, ITERATIONS, M, N, K, true),
+        gemm_gpu(_fp32_, ITERATIONS, M, N, K, true),
+        gemm_gpu(_fp64_, ITERATIONS, M, N, K, true)};
+    struct dT_Times gpuTimes_OffloadAlways = {
+        gemm_gpu(_fp16_, ITERATIONS, M, N, K, false),
+        gemm_gpu(_fp32_, ITERATIONS, M, N, K, false),
+        gemm_gpu(_fp64_, ITERATIONS, M, N, K, false)};
 
-  // Perform all GPU kernels & record results
-  if (GPU_ENABLED) {
+    // Compare results: when GPU > CPU record value
+
+    // Save values to CSV (dimensions, time taken, GFLOP/s, etc)
+
+    // Increment Dimensions
   }
 
   return 0;

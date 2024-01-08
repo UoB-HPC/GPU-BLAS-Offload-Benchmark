c97d0c163e979663adb0b74548ebbdf68c715b42
Finn Wilkinson <finnwilkinson5@gmail.com>
2024-01-08 10:19:37 +0000

    Renamed flags.h to utilities.h

--- utilities.h
+++ utilities.h
@@ -1,52 +1,43 @@
 #pragma once
 
+#include <math.h>
+#include <omp.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
-#include <omp.h>
 #include <string.h>
-#include <math.h>
+#include <sys/time.h>
 
 // Define CPU related macros
-#if !defined CPU_ARMPL && !defined CPU_ONEMKL && !defined CPU_AOCL &&          \
-    !defined CPU_OPENBLAS
-#define CPU_DEFAULT
-#endif
-
-#if defined CPU_DEFAULT
-#define CPU_LIB_NAME "None"
-#elif defined CPU_ARMPL
+#if defined CPU_ARMPL
 #define CPU_LIB_NAME "Arm Performance Libraries"
-#include <armpl.h>
 #elif defined CPU_ONEMKL
 #define CPU_LIB_NAME "Intel OneMKL"
 #elif defined CPU_OPENBLAS
 #define CPU_LIB_NAME "OpenBLAS"
 #elif defined CPU_AOCL
 #define CPU_LIB_NAME "AMD Optimized CPU Libraries"
+#else
+#define CPU_DEFAULT
+#define CPU_LIB_NAME "None"
 #endif
 
 // Define GPU related macros
-#if !defined GPU_CUBLAS && !defined GPU_ONEMKL && !defined GPU_ROCBLAS
-#define GPU_DEFAULT
-#endif
-
-#if defined GPU_DEFAULT
-#define GPU_LIB_NAME "None"
-#elif defined GPU_CUBLAS
+#if defined GPU_CUBLAS
 #define GPU_LIB_NAME "NVIDIA cuBLAS"
 #elif defined GPU_ONEMKL
 #define GPU_LIB_NAME "Intel OneMKL"
 #elif defined GPU_ROCBLAS
 #define GPU_LIB_NAME "AMD rocBLAS"
+#else
+#define GPU_DEFAULT
+#define GPU_LIB_NAME "None"
+#define GPU_ENABLED false
 #endif
 
-#ifndef GPU_DEFAULT
+#ifndef GPU_ENABLED
 #define GPU_ENABLED true
-#else
-#define GPU_ENABLED false
 #endif
 
 // Define general macros
@@ -71,4 +62,4 @@ double calcGflops(const uint64_t flops, const double seconds) {
 }
 
 // Define data type enums
-typedef enum {_fp32_, _fp64_ } dataTypes;
+typedef enum { _fp32_, _fp64_ } dataTypes;

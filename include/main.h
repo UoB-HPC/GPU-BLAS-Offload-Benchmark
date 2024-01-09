81952f69ccee5a7a149cfe5db565f17bfcc224e8
Finn Wilkinson <finnwilkinson5@gmail.com>
2024-01-09 16:57:06 +0000

    Added SGEMM rectangualr problem sizes.

--- include/main.h
+++ include/main.h
@@ -44,47 +44,28 @@ void printBenchmarkConfig(const int iters, const int upperLimit) {
 /** A function to open a new csv file in WRITE mode and write the standard
  * headers to the file.
  * Returns the file pointer. */
-FILE *newCSV(const char *filename) {
-  FILE *newFile;
-  newFile = fopen(filename, "w");
-  if (newFile == NULL) {
-    perror("ERROR - File failed to open: ");
-    exit(1);
-  }
-  // Write headers to file.
-  fprintf(newFile,
-          "Device,Kernel,M,N,K,Total Problem Size (KiB),Iterations,Total "
-          "Seconds,GFLOP/s\n");
-  return newFile;
-}
+FILE *newCSV(const char *filename);
 
 /** A function to write a new line to an open CSV file. */
 void writeLineToCsv(FILE *fptr, const char *device, const char *kernel,
                     const int M, const int N, const int K,
                     const double totalProbSize, const int iters,
-                    const double totalTime, const double gflops) {
-  if (fptr == NULL) {
-    printf("ERROR - Attempted to write line to invalid file pointer.\n");
-    exit(1);
-  }
-  fprintf(fptr, "%s,%s,%d,%d,%d,%.2lf,%d,%lf,%lf\n", device, kernel, M, N, K,
-          totalProbSize, iters, totalTime, gflops);
-}
+                    const double totalTime, const double gflops);
 
 /** A function to calculate GFLOPs. */
-double calcGflops(const uint64_t flops, const int iters, const double seconds) {
-  return (seconds == 0.0 || seconds == INFINITY)
-             ? 0.0
-             : ((double)(flops * iters) / seconds) * 1e-9;
-}
+double calcGflops(const uint64_t flops, const int iters, const double seconds);
 
 /** A function to calculate KiB from a data-structur's dimensions. */
-double calcKib(const uint64_t probSize, const uint64_t bytesPerElem) {
-  return ((double)(probSize * bytesPerElem) / 1024);
-}
+double calcKib(const uint64_t probSize, const uint64_t bytesPerElem);
 
 /** A function for calculating FLOPs performed by a GEMM. */
-uint64_t gemmFlops(const int M, const int N, const int K) {
+uint64_t gemmFlops(const int M, const int N, const int K);
 
-  return ((uint64_t)M * (uint64_t)N * (uint64_t)K * 2);
-}
+/** A function for calculating the total GEMM problem size in KiB. */
+double gemmKib(const dataTypes type, const int M, const int N, const int K);
+
+/** A function to parse a string to integer. */
+int parseInt(const char *str);
+
+/** A function which parsen the runtime arguments. */
+void getParameters(int argc, char *argv[]);
--- src/main.c
+++ src/main.c
@@ -4,9 +4,6 @@ int iters = 10;
 int upperLimit = 128;
 FILE *fptr;
 
-int parseInt(const char *str);
-void getParameters(int argc, char *argv[]);
-
 int main(int argc, char *argv[]) {
   getParameters(argc, argv);
   printBenchmarkConfig(iters, upperLimit);
@@ -17,36 +14,133 @@ int main(int argc, char *argv[]) {
     mkdir(CSV_DIR, 0700);
   }
 
-  // SGEMM Comparison - Square
+  // SGEMM Comparison
   printf("Comparing SGEMM Kernels:\n");
   printf("\tSquare Problem Sizes...\n");
   fptr = newCSV(CSV_DIR "/sgemm_square.csv");
   for (int dim = 1; dim <= upperLimit; dim++) {
     const int M = dim, N = dim, K = dim;
-    const uint64_t gemmProbSize = (M * K) + (K * N) + (M * N);
+    const uint64_t gemmProbSize = gemmKib(_fp32_, M, N, K);
     // Perform CPU
     double cpuTime = gemm_cpu(_fp32_, iters, M, N, K);
-    writeLineToCsv(fptr, "cpu", "sgemm", M, N, K, calcKib(gemmProbSize, 4),
-                   iters, cpuTime,
+    writeLineToCsv(fptr, "cpu", "sgemm", M, N, K, gemmProbSize, iters, cpuTime,
                    calcGflops(gemmFlops(M, N, K), iters, cpuTime));
 
     // Perform GPU
     // - Offload to/from GPU once before all iterations and once after
     double gpuTime_once = gemm_gpu(_fp32_, iters, M, N, K, true);
-    writeLineToCsv(fptr, "gpu_offloadOnce", "sgemm", M, N, K,
-                   calcKib(gemmProbSize, 4), iters, gpuTime_once,
+    writeLineToCsv(fptr, "gpu_offloadOnce", "sgemm", M, N, K, gemmProbSize,
+                   iters, gpuTime_once,
                    calcGflops(gemmFlops(M, N, K), iters, gpuTime_once));
     // - Offload to/from GPU every iteration
     double gpuTime_every = gemm_gpu(_fp32_, iters, M, N, K, false);
-    writeLineToCsv(fptr, "gpu_offloadAlways", "sgemm", M, N, K,
-                   calcKib(gemmProbSize, 4), iters, gpuTime_every,
+    writeLineToCsv(fptr, "gpu_offloadAlways", "sgemm", M, N, K, gemmProbSize,
+                   iters, gpuTime_every,
                    calcGflops(gemmFlops(M, N, K), iters, gpuTime_every));
   }
   // Close file
   fclose(fptr);
 
-  // MxK = 16Mx1K, 1Mx16K, Mx32, 32xM,
+  printf("\tRectangular Problem Sizes:\n");
+  printf("\t\tTall and thin (16M x K)...\n");
+  fptr = newCSV(CSV_DIR "/sgemm_rectangular_16MxK.csv");
+  for (int dim = 16; dim <= upperLimit; dim += 16) {
+    const int M = dim, N = dim, K = (dim / 16);
+    const uint64_t gemmProbSize = gemmKib(_fp32_, M, N, K);
+    // Perform CPU
+    double cpuTime = gemm_cpu(_fp32_, iters, M, N, K);
+    writeLineToCsv(fptr, "cpu", "sgemm", M, N, K, gemmProbSize, iters, cpuTime,
+                   calcGflops(gemmFlops(M, N, K), iters, cpuTime));
 
+    // Perform GPU
+    // - Offload to/from GPU once before all iterations and once after
+    double gpuTime_once = gemm_gpu(_fp32_, iters, M, N, K, true);
+    writeLineToCsv(fptr, "gpu_offloadOnce", "sgemm", M, N, K, gemmProbSize,
+                   iters, gpuTime_once,
+                   calcGflops(gemmFlops(M, N, K), iters, gpuTime_once));
+    // - Offload to/from GPU every iteration
+    double gpuTime_every = gemm_gpu(_fp32_, iters, M, N, K, false);
+    writeLineToCsv(fptr, "gpu_offloadAlways", "sgemm", M, N, K, gemmProbSize,
+                   iters, gpuTime_every,
+                   calcGflops(gemmFlops(M, N, K), iters, gpuTime_every));
+  }
+  // Close file
+  fclose(fptr);
+
+  printf("\t\tTall and thin (M x 32)...\n");
+  fptr = newCSV(CSV_DIR "/sgemm_rectangular_Mx32.csv");
+  for (int dim = 1; dim <= upperLimit; dim++) {
+    const int M = dim, N = dim, K = 32;
+    const uint64_t gemmProbSize = gemmKib(_fp32_, M, N, K);
+    // Perform CPU
+    double cpuTime = gemm_cpu(_fp32_, iters, M, N, K);
+    writeLineToCsv(fptr, "cpu", "sgemm", M, N, K, gemmProbSize, iters, cpuTime,
+                   calcGflops(gemmFlops(M, N, K), iters, cpuTime));
+
+    // Perform GPU
+    // - Offload to/from GPU once before all iterations and once after
+    double gpuTime_once = gemm_gpu(_fp32_, iters, M, N, K, true);
+    writeLineToCsv(fptr, "gpu_offloadOnce", "sgemm", M, N, K, gemmProbSize,
+                   iters, gpuTime_once,
+                   calcGflops(gemmFlops(M, N, K), iters, gpuTime_once));
+    // - Offload to/from GPU every iteration
+    double gpuTime_every = gemm_gpu(_fp32_, iters, M, N, K, false);
+    writeLineToCsv(fptr, "gpu_offloadAlways", "sgemm", M, N, K, gemmProbSize,
+                   iters, gpuTime_every,
+                   calcGflops(gemmFlops(M, N, K), iters, gpuTime_every));
+  }
+  // Close file
+  fclose(fptr);
+
+  printf("\t\tShort and wide (M x 16K)...\n");
+  fptr = newCSV(CSV_DIR "/sgemm_rectangular_Mx16K.csv");
+  for (int dim = 16; dim <= upperLimit; dim += 16) {
+    const int M = (dim / 16), N = (dim / 16), K = dim;
+    const uint64_t gemmProbSize = gemmKib(_fp32_, M, N, K);
+    // Perform CPU
+    double cpuTime = gemm_cpu(_fp32_, iters, M, N, K);
+    writeLineToCsv(fptr, "cpu", "sgemm", M, N, K, gemmProbSize, iters, cpuTime,
+                   calcGflops(gemmFlops(M, N, K), iters, cpuTime));
+
+    // Perform GPU
+    // - Offload to/from GPU once before all iterations and once after
+    double gpuTime_once = gemm_gpu(_fp32_, iters, M, N, K, true);
+    writeLineToCsv(fptr, "gpu_offloadOnce", "sgemm", M, N, K, gemmProbSize,
+                   iters, gpuTime_once,
+                   calcGflops(gemmFlops(M, N, K), iters, gpuTime_once));
+    // - Offload to/from GPU every iteration
+    double gpuTime_every = gemm_gpu(_fp32_, iters, M, N, K, false);
+    writeLineToCsv(fptr, "gpu_offloadAlways", "sgemm", M, N, K, gemmProbSize,
+                   iters, gpuTime_every,
+                   calcGflops(gemmFlops(M, N, K), iters, gpuTime_every));
+  }
+  // Close file
+  fclose(fptr);
+
+  printf("\t\tShort and wide (32 x K)...\n");
+  fptr = newCSV(CSV_DIR "/sgemm_rectangular_32xK.csv");
+  for (int dim = 1; dim <= upperLimit; dim++) {
+    const int M = 32, N = 32, K = dim;
+    const uint64_t gemmProbSize = gemmKib(_fp32_, M, N, K);
+    // Perform CPU
+    double cpuTime = gemm_cpu(_fp32_, iters, M, N, K);
+    writeLineToCsv(fptr, "cpu", "sgemm", M, N, K, gemmProbSize, iters, cpuTime,
+                   calcGflops(gemmFlops(M, N, K), iters, cpuTime));
+
+    // Perform GPU
+    // - Offload to/from GPU once before all iterations and once after
+    double gpuTime_once = gemm_gpu(_fp32_, iters, M, N, K, true);
+    writeLineToCsv(fptr, "gpu_offloadOnce", "sgemm", M, N, K, gemmProbSize,
+                   iters, gpuTime_once,
+                   calcGflops(gemmFlops(M, N, K), iters, gpuTime_once));
+    // - Offload to/from GPU every iteration
+    double gpuTime_every = gemm_gpu(_fp32_, iters, M, N, K, false);
+    writeLineToCsv(fptr, "gpu_offloadAlways", "sgemm", M, N, K, gemmProbSize,
+                   iters, gpuTime_every,
+                   calcGflops(gemmFlops(M, N, K), iters, gpuTime_every));
+  }
+  // Close file
+  fclose(fptr);
   printf("Finished!\n");
 
   // DGEMM Comparison - Square
@@ -57,25 +151,81 @@ int main(int argc, char *argv[]) {
     int M = dim, N = dim, K = dim;
     // Perform CPU
     double cpuTime = gemm_cpu(_fp64_, iters, M, N, K);
-    uint64_t gemmProbSize = (M * K) + (K * N) + (M * N);
-    writeLineToCsv(fptr, "cpu", "dgemm", M, N, K, calcKib(gemmProbSize, 8),
-                   iters, cpuTime,
+    uint64_t gemmProbSize = gemmKib(_fp32_, M, N, K);
+    writeLineToCsv(fptr, "cpu", "dgemm", M, N, K, gemmProbSize, iters, cpuTime,
                    calcGflops(gemmFlops(M, N, K), iters, cpuTime));
     // Perform GPU
     // - Offload to/from GPU once before all iterations and once after
     double gpuTime_once = gemm_gpu(_fp64_, iters, M, N, K, true);
-    writeLineToCsv(fptr, "gpu_offloadOnce", "dgemm", M, N, K,
-                   calcKib(gemmProbSize, 8), iters, gpuTime_once,
+    writeLineToCsv(fptr, "gpu_offloadOnce", "dgemm", M, N, K, gemmProbSize,
+                   iters, gpuTime_once,
                    calcGflops(gemmFlops(M, N, K), iters, gpuTime_once));
     // - Offload to/from GPU every iteration
     double gpuTime_every = gemm_gpu(_fp64_, iters, M, N, K, false);
-    writeLineToCsv(fptr, "gpu_offloadAlways", "dgemm", M, N, K,
-                   calcKib(gemmProbSize, 8), iters, gpuTime_every,
+    writeLineToCsv(fptr, "gpu_offloadAlways", "dgemm", M, N, K, gemmProbSize,
+                   iters, gpuTime_every,
                    calcGflops(gemmFlops(M, N, K), iters, gpuTime_every));
   }
   printf("Finished!\n");
 
   return 0;
+}
+
+double calcGflops(const uint64_t flops, const int iters, const double seconds) {
+  return (seconds == 0.0 || seconds == INFINITY)
+             ? 0.0
+             : ((double)(flops * iters) / seconds) * 1e-9;
+}
+
+double calcKib(const uint64_t probSize, const uint64_t bytesPerElem) {
+  return ((double)(probSize * bytesPerElem) / 1024);
+}
+
+uint64_t gemmFlops(const int M, const int N, const int K) {
+  return ((uint64_t)M * (uint64_t)N * (uint64_t)K * 2);
+}
+
+double gemmKib(const dataTypes type, const int M, const int N, const int K) {
+  uint8_t bytes = 0;
+  switch (type) {
+  case _fp32_:
+    bytes = 4;
+    break;
+  case _fp64_:
+    bytes = 8;
+    break;
+  default:
+    break;
+  }
+  uint64_t M_ = (uint64_t)M, N_ = (uint64_t)N, K_ = (uint64_t)K;
+  uint64_t probSize = (M_ * K_) + (K_ * N_) + (M_ * N_);
+  return calcKib(probSize, bytes);
+}
+
+FILE *newCSV(const char *filename) {
+  FILE *newFile;
+  newFile = fopen(filename, "w");
+  if (newFile == NULL) {
+    perror("ERROR - File failed to open: ");
+    exit(1);
+  }
+  // Write headers to file.
+  fprintf(newFile,
+          "Device,Kernel,M,N,K,Total Problem Size (KiB),Iterations,Total "
+          "Seconds,GFLOP/s\n");
+  return newFile;
+}
+
+void writeLineToCsv(FILE *fptr, const char *device, const char *kernel,
+                    const int M, const int N, const int K,
+                    const double totalProbSize, const int iters,
+                    const double totalTime, const double gflops) {
+  if (fptr == NULL) {
+    printf("ERROR - Attempted to write line to invalid file pointer.\n");
+    exit(1);
+  }
+  fprintf(fptr, "%s,%s,%d,%d,%d,%.2lf,%d,%lf,%lf\n", device, kernel, M, N, K,
+          totalProbSize, iters, totalTime, gflops);
 }
 
 int parseInt(const char *str) {

3296ac9b3f9d762e9ecf15505f5bb2a5e07b9003
Finn Wilkinson <finnwilkinson5@gmail.com>
2024-01-10 14:56:28 +0000

    ArmPL fix.

--- ArmPL/gemm.c
+++ ArmPL/gemm.c
@@ -34,7 +34,7 @@ double gemm_cpu(const dataTypes dType, const int iters, const int m,
     // Perform all SGEMM iterations
     for (int i = 0; i < iters; i++) {
       cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, ALPHA, A,
-                  MAX(1, m), B, MAX(1, k), BETA, C, MAX(1, m));
+                  MAX(1, k), B, MAX(1, n), BETA, C, MAX(1, n));
       // Call to `consume()` function to ensure all iterations are performed
       consume((void *)A, (void *)B, (void *)C);
     }
@@ -64,14 +64,14 @@ double gemm_cpu(const dataTypes dType, const int iters, const int m,
     }
     // Warmup run
     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, ALPHA, A,
-                MAX(1, m), B, MAX(1, k), BETA, C, MAX(1, m));
+                MAX(1, k), B, MAX(1, n), BETA, C, MAX(1, n));
     consume((void *)A, (void *)B, (void *)C);
     // Start timer
     gettimeofday(&start_tv, NULL);
     // Perform all SGEMM iterations
     for (int i = 0; i < iters; i++) {
       cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, ALPHA, A,
-                  MAX(1, m), B, MAX(1, k), BETA, C, MAX(1, m));
+                  MAX(1, k), B, MAX(1, n), BETA, C, MAX(1, n));
       // Call to `consume()` function to ensure all iterations are performed
       consume((void *)A, (void *)B, (void *)C);
     }

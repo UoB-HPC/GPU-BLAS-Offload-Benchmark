cb4eed79166450eb983b209fa47de3a3b4ee7d1e
Finn Wilkinson <finnwilkinson5@gmail.com>
2024-03-13 17:46:41 +0000

    Tried to fix async exception handler.

--- oneMKL/GPU/common.hh
+++ oneMKL/GPU/common.hh
@@ -8,16 +8,16 @@
 #include <sycl/sycl.hpp>
 
 // Create an exception handler for asynchronous SYCL exceptions
-static auto exception_handler = [](sycl::exception_list e_list,
-                                   std::string kernel) {
-  for (std::exception_ptr const& e : e_list) {
-    try {
-      std::rethrow_exception(e);
-    } catch (std::exception const& e) {
-      std::cout << "ERROR -  Caught asynchronous SYCL exception during "
-                << kernel << ": " << e.what() << std::endl;
-    }
-  }
-};
+static const std::function<void(sycl::exception_list)> exception_handler =
+    [](sycl::exception_list e_list, std::string kernel) {
+      for (std::exception_ptr const& e : e_list) {
+        try {
+          std::rethrow_exception(e);
+        } catch (std::exception const& e) {
+          std::cout << "ERROR -  Caught asynchronous SYCL exception during "
+                    << kernel << ": " << e.what() << std::endl;
+        }
+      }
+    };
 
 #endif

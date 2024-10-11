66263aa04bcdaaebca985e41063d270c9a7447c6
Alex Cockrean <84676155+ABenC377@users.noreply.github.com>
2024-10-11 15:39:46 +0100

    rebasing

--- include/kernels/gemm.hh
+++ include/kernels/gemm.hh
@@ -103,14 +103,8 @@ class gemm {
 
     // Using a=0.45 and b=c=0.22 as default probabilities
     for (int i = 0; i < edges; i++) {
-      while (!rMat(A_, n_, 0, n_ - 1, 0, n_ - 1,
-                   0.45, 0.22, 0.22,
-                   &gen, dist, false)) {}
-    }
-    for (int i = 0; i < edges; i++) {
-      while (!rMat(B_, n_, 0, n_ - 1, 0, n_ - 1,
-                   0.45, 0.22, 0.22,
-                   &gen, dist, false)) {}
+      rMat(A_, n_, 0, n_ - 1, 0, n_ - 1, 0.45, 0.22, 0.22, &gen, dist, false);
+      rMat(B_, n_, 0, n_ - 1, 0, n_ - 1, 0.45, 0.22, 0.22, &gen, dist, false);
     }
   }
 
@@ -118,23 +112,18 @@ class gemm {
   void callConsume() { consume((void*)A_, (void*)B_, (void*)C_); }
 
   /** Recursive function to populate sparse matrices */
-  bool rMat(T* M, int n, int x1, int x2, int y1, int y2,
-            float a, float b, float c, std::default_random_engine* gen,
+  void rMat(T* M, int n, int x1, int x2, int y1, int y2, float a, float b,
+            float c, std::default_random_engine* gen,
             std::uniform_real_distribution<double> dist, bool bin) {
     // If a 1x1 submatrix, then add an edge and return out
     if (x1 >= x2 && y1 >= y2) {
-      if (abs(M[(y1 * n) + x1]) > 0.1) {
-        return false;
-      } else {
-        // Add 1.0 if this is a binary graph, and a random real number otherwise
-        M[(int) (y1 * n) + x1] = (bin) ? 1.0 : (((rand() % 10000) /
+      M[(int) (y1 * n) + x1] = (bin) ? 1.0 : (((rand() % 10000) /
                                                  100.0) - 50.0);
-        return true;
-      }
+      return;
     } else {
       // Divide up the matrix
-      int xMidPoint = x1 + floor((x2 - x1) / 2);
-      int yMidPoint = y1 + floor((y2 - y1) / 2);
+      int xMidPoint = (x1 == x2) ? x1 : x1 + floor((x2 - x1) / 2);
+      int yMidPoint = (y1 == y2) ? y1 : y1 + floor((y2 - y1) / 2);
 
       // ToDo -- add some noise to these values between iterations
       float newA = a;
@@ -142,25 +131,23 @@ class gemm {
       float newC = c;
 
       // Work out which quarter to recurse into
-      // There are some ugly ternary operators here to avoid going out of bounds in the edge case
-      // that we are already at 1 width or 1 height
+      // There are some ugly ternary operators here to avoid going out of
+      // bounds in the edge case that we are already at 1 width or 1 height
       float randomNum = dist(*gen);
       if (randomNum < a) {
-        return rMat(M, n, x1, xMidPoint, y1, yMidPoint,
-                    newA, newB, newC, gen, dist, bin);
+        rMat(M, n, x1, xMidPoint, y1, yMidPoint, newA, newB, newC, gen, dist,
+             bin);
       } else if (randomNum < (a + b)) {
-        return rMat(M, n, ((xMidPoint < x2) ? xMidPoint + 1 : xMidPoint), x2, y1, yMidPoint,
-                    newA, newB, newC, gen, dist, bin);
+        rMat(M, n, xMidPoint, x2, y1, yMidPoint, newA, newB, newC, gen, dist,
+             bin);
       } else if (randomNum < (a + b + c)) {
-        return rMat(M, n, x1, xMidPoint, ((yMidPoint < y2) ? yMidPoint + 1 : yMidPoint), y2,
-                    newA, newB, newC, gen, dist, bin);
+        rMat(M, n, x1, xMidPoint,  yMidPoint, y2, newA, newB, newC, gen,
+             dist, bin);
       } else {
-        return rMat(M, n, ((xMidPoint < x2) ? xMidPoint + 1 : xMidPoint), x2,
-                    ((yMidPoint < y2) ? yMidPoint + 1 : yMidPoint), y2, newA, newB, newC,
-                    gen, dist, bin);
+        rMat(M, n, xMidPoint, x2, yMidPoint, y2, newA,  newB, newC, gen,
+             dist, bin);
       }
     }
-    return true;
   }
 
   void toCSR(T* dense, int n_col, int n_row, int nnz, T* vals, int* col_index,

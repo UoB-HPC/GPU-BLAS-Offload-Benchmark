5ccfe251b389ed4b4f79c2a5f0ee4932d56311ea
Alex Cockrean <84676155+ABenC377@users.noreply.github.com>
2025-10-06 12:12:38 +0100

    Getting rid of signed/unsigned comparison warnings

--- AOCL/spmm.hh
+++ AOCL/spmm.hh
@@ -31,13 +31,7 @@ public:
     using spmm<T>::C_nnz_;
 
     void initialise(int m, int n, int k, double sparsity, matrixType type, 
-                    bool binary = false) {
-      if (print_) {
-          std::cout << "===========  CPU   ===========" << std::endl;
-      }
-
-      if (print_) std::cout << "setting up metadata" << std::endl;
-      
+                    bool binary = false) {      
       sparsity_ = sparsity;
       type_ = type;
       
@@ -52,76 +46,23 @@ public:
       nnzB_aocl_ = B_nnz_ = 1 + (uint64_t)((double)total_elements_B * (1.0 - sparsity));
       C_allocated = false;
       
-      if (print_) std::cout << "Allocating dense matrix arrays" << std::endl;
-
       base_ = aoclsparse_index_base_zero;
       operationA_ = aoclsparse_operation_none;
       operationB_ = aoclsparse_operation_none;
 
-      if (print_) std::cout << "Creating AOCL description for A" << std::endl;
       status_ = aoclsparse_create_mat_descr(&A_description_);
       if (status_ != aoclsparse_status_success) {
-        std::cerr << "aoclsparse_create_mat_descr failing for A" << std::endl;
         printAOCLError(status_);
       }
-      if (print_) std::cout << "Creating AOCL description for B" << std::endl;
       status_ = aoclsparse_create_mat_descr(&B_description_);
       if (status_ != aoclsparse_status_success) {
-        std::cerr << "aoclsparse_create_mat_descr failing for B" << std::endl;
         printAOCLError(status_);
       }
-      if (print_) std::cout << "initialising matrices" << std::endl;
       initInputMatrices();
-
-      if (print_) {
-        std::cout << "===============Initialised=================" << std::endl;
-        std::cout << "___________________________________________" << std::endl;
-        std::cout << "A nnz = " << A_nnz_ << std::endl;
-        std::cout << "A rows = [";
-        for (int64_t i = 0; i < (m_ + 1); i++) {
-          std::cout << A_rows_[i];
-          if (i < m_) std::cout << ", ";
-        }
-        std::cout << "]" << std::endl;
-        std::cout << "A cols = [";
-        for (int64_t i = 0; i < A_nnz_; i++) {
-          std::cout << A_cols_[i];
-          if (i < (A_nnz_ - 1)) std::cout << ", ";
-        }
-        std::cout << "]" << std::endl;
-        std::cout << "A vals = [";
-        for (int64_t i = 0; i < A_nnz_; i++) {
-          std::cout << A_vals_[i];
-          if (i < (A_nnz_ - 1)) std::cout << ", ";
-        }
-        std::cout << "]" << std::endl;
-        
-        std::cout << "B nnz = " << B_nnz_ << std::endl;
-        std::cout << "B rows = [";
-        for (int64_t i = 0; i < (k_ + 1); i++) {
-          std::cout << B_rows_[i];
-          if (i < k_) std::cout << ", ";
-        }
-        std::cout << "]" << std::endl;
-        std::cout << "B cols = [";
-        for (int64_t i = 0; i < B_nnz_; i++) {
-          std::cout << B_cols_[i];
-          if (i < (B_nnz_ - 1)) std::cout << ", ";
-        }
-        std::cout << "]" << std::endl;
-        std::cout << "B vals = [";
-        for (int64_t i = 0; i < B_nnz_; i++) {
-          std::cout << B_vals_[i];
-          if (i < (B_nnz_ - 1)) std::cout << ", ";
-        }
-        std::cout << "]" << std::endl << std::endl;
-        std::cout << "___________________________________________" << std::endl;
-      }
     }
 
 protected:
     void toSparseFormat() override {
-      if (print_) std::cout << "Allocating CSR Arrays for A" << std::endl;
       A_rows_ = (aoclsparse_int*)calloc(m_ + 1, sizeof(aoclsparse_int));
       A_cols_ = (aoclsparse_int*)calloc(nnzA_aocl_, sizeof(aoclsparse_int));
       A_vals_ = (T*)calloc(nnzA_aocl_, sizeof(T));
@@ -131,7 +72,6 @@ protected:
       }
 
       // Initialise datastructures for the CSR format
-      if (print_) std::cout << "Allocating CSR Arrays for B" << std::endl;
       B_rows_ = (aoclsparse_int*)calloc(k_ + 1, sizeof(aoclsparse_int));
       B_cols_ = (aoclsparse_int*)calloc(nnzB_aocl_, sizeof(aoclsparse_int));
       B_vals_ = (T*)calloc(nnzB_aocl_, sizeof(T)); 
@@ -140,7 +80,6 @@ protected:
         exit(1);
       }
 
-      if (print_) std::cout << "Generating R-MAT matrix for A" << std::endl;
       int seedOffset = 0;
       do {
         if (type_ == matrixType::rmat) {
@@ -152,11 +91,10 @@ protected:
         } else {
           std::cerr << "Matrix type not supported" << std::endl;
           exit(1);
-        } while (calcCNNZ<aoclsparse_int>(m_, A_nnz_, A_rows_, A_cols_, k_, B_nnz_, B_rows_, B_cols_) == 0);
-      }
+        } 
+      } while (calcCNNZ<aoclsparse_int>(m_, A_nnz_, A_rows_, A_cols_, k_, B_nnz_, B_rows_, B_cols_) == 0);
 
       // Move into the AOCL CSR matrix handle
-      if (print_) std::cout << "Creating AOCL CSR matrix for A" << std::endl;
       if constexpr (std::is_same_v<T, float>) {
         status_ = aoclsparse_create_scsr(&A_aocl_, 
                                          base_, 
@@ -182,7 +120,6 @@ protected:
       }
 
       // Now sort the matrix -- needed for this AOCL function
-      if (print_) std::cout << "Sorting AOCL CSR matrix for A" << std::endl;
       status_ = aoclsparse_order_mat(A_aocl_);
       if (status_ != aoclsparse_status_success) {
         std::cerr << "aoclsparse_order_mat for A is failing with problem size of " << m_ << "x" << k_ << " . " << k_ << "x" << n_ << std::endl;
@@ -190,7 +127,6 @@ protected:
       }
       
       // Move into the AOCL CSR matrix handle
-      if (print_) std::cout << "Creating AOCL CSR matrix for B" << std::endl;
       if constexpr (std::is_same_v<T, float>) {
         status_ = aoclsparse_create_scsr(&B_aocl_, 
                                          base_, 
@@ -216,7 +152,6 @@ protected:
       }
 
       // Now sort the matrix -- needed for this AOCL function
-      if (print_) std::cout << "Sorting AOCL CSR matrix for B" << std::endl;
       status_ = aoclsparse_order_mat(B_aocl_);
       if (status_ != aoclsparse_status_success) {
         std::cerr << "aoclsparse_order_mat for B is failing with problem size of " << m_ << "x" << k_ << " . " << k_ << "x" << n_ << std::endl;
@@ -230,7 +165,6 @@ private:
 
     void callSpmm() override {
       if (C_allocated) {
-        if (print_) std::cout << "Freeing C arrays" << std::endl;
         if (C_vals_ != nullptr) {
           free(C_vals_);
           C_vals_ = nullptr;
@@ -247,7 +181,6 @@ private:
       }
 
       request_ = aoclsparse_stage_nnz_count;
-      if (print_) std::cout << "Calculating number of non-zeros in C" << std::endl;
       status_ = aoclsparse_sp2m(operationA_, 
                                 A_description_, 
                                 A_aocl_, 
@@ -262,7 +195,6 @@ private:
       }
 
       request_ = aoclsparse_stage_finalize;
-      if (print_) std::cout << "SpMM compute" << std::endl;
       status_ = aoclsparse_sp2m(operationA_, 
                                 A_description_, 
                                 A_aocl_, 
@@ -276,7 +208,6 @@ private:
         printAOCLError(status_);
       }
 
-      if (print_) std::cout << "Exporting CSR arrays for C" << std::endl;
       if constexpr (std::is_same_v<T, float>) {
         status_ = aoclsparse_export_scsr(C_aocl_, 
                                          &base_,
@@ -308,47 +239,39 @@ private:
     }
 
     void postCallKernelCleanup() override {
-      if (print_) std::cout << "Destroying A description" << std::endl;
       status_ = aoclsparse_destroy_mat_descr(A_description_);
       if (status_ != aoclsparse_status_success) {
         std::cerr << "aoclsparse_destroy_mat_descr failing for A_description_" << std::endl;
         printAOCLError(status_);
       }
-      if (print_) std::cout << "Destroying B description" << std::endl;
       status_ = aoclsparse_destroy_mat_descr(B_description_);
       if (status_ != aoclsparse_status_success) {
         std::cerr << "aoclsparse_destroy_mat_descr failing for B_description_" << std::endl;
         printAOCLError(status_);
       }
 
-      if (print_) std::cout << "Destroying A aocl matrix handle" << std::endl;
       status_ = aoclsparse_destroy(&A_aocl_);
       if (status_ != aoclsparse_status_success) {
         std::cerr << "aoclsparse_destroy failing for A" << std::endl;
         printAOCLError(status_);
       }
-      if (print_) std::cout << "Destroying B aocl matrix handle" << std::endl;
       status_ = aoclsparse_destroy(&B_aocl_);
       if (status_ != aoclsparse_status_success) {
         std::cerr << "aoclsparse_destroy failing for B" << std::endl;
         printAOCLError(status_);
       }
 
-      if (print_) std::cout << "Destroying C aocl matrix handle" << std::endl;
       status_ = aoclsparse_destroy(&C_aocl_);
       if (status_ != aoclsparse_status_success) {
         std::cerr << "aoclsparse_destroy failing for C" << std::endl;
         printAOCLError(status_);
       }
-      if (print_) std::cout << "Freeing CSR arrays for A" << std::endl;
       free(A_rows_);
       free(A_cols_);
       free(A_vals_);
-      if (print_) std::cout << "Freeing CSR arrays for B" << std::endl;
       free(B_rows_);
       free(B_cols_);
       free(B_vals_);
-      if (print_) std::cout << "AOCL cleanup complete" << std::endl;
     }
 
     void printAOCLError(aoclsparse_status stat) {
@@ -405,8 +328,6 @@ private:
       std::cerr << std::endl;
       exit(1);
     }
-
-    bool print_ = false;
 
     aoclsparse_status status_;
 
